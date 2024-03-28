0ea7e24110b9840e8bd062db2512f8eefee91d37
Noxurge <renan.medeiros9@gmail.com>
2024-03-27 22:33:33 -0300

    fix js

--- js/typing.js
+++ js/typing.js
@@ -1,13 +1,15 @@
-var title = "Welcome to Noxurge Room!";
-var titleContainer = document.getElementById('typing-title');
-var i = 0;
+document.addEventListener('DOMContentLoaded', function() {
+	var title = "Welcome to Noxurge Room!";
+	var titleContainer = document.getElementById('typing-title');
+	var i = 0;
 
-function typeTitle() {
-	    if (i < title.length) {
-				        titleContainer.innerHTML += title.charAt(i);
-				        i++;
-				        setTimeout(typeTitle, 100);
-				    }
-}
+	function typeTitle() {
+		    if (i < title.length) {
+					        titleContainer.innerHTML += title.charAt(i);
+					        i++;
+					        setTimeout(typeTitle, 100);
+					    }
+	}
 
-window.onload = typeTitle;
+	typeTitle();
+});
