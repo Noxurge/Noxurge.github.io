c1e2fa06a330573ab73fc3a842e4a59c17e071b7
Noxurge <renan.medeiros9@gmail.com>
2024-03-27 22:22:16 -0300

    folder JS created

---
+++ js/typing.js
@@ -0,0 +1,13 @@
+var title = "Welcome to Noxurge Room!";
+var titleContainer = document.getElementById('typing-title');
+var i = 0;
+
+function typeTitle() {
+	    if (i < title.length) {
+				        titleContainer.innerHTML += title.charAt(i);
+				        i++;
+				        setTimeout(typeTitle, 100);
+				    }
+}
+
+window.onload = typeTitle;
